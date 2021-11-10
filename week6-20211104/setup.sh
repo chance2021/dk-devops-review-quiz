0123f85af872874b1b5b9640f1860c6b094b6c94
chance2021 <61915798+chance2021@users.noreply.github.com>
2021-11-10 06:36:43 -0500

    Update setup.sh

--- week6-20211104/setup.sh
+++ week6-20211104/setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+docker pull nginx
+docker pull redis
+docker pull hello-world
+docker pull ubuntu
 docker run -d nginx &
 docker run -d redis &
+clear
+echo "Please wait 30s for the environment setup..."
+sleep 30s
+clear
+echo "The test environment is ready! Please go to the quiz to start your test! Good luck!"
