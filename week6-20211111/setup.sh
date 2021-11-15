92faac99a87b91e238082b52c33033a19c0b5fdd
chance <cchen@indocresearch.org>
2021-11-15 07:37:50 -0500

    week6 quiz setup

---
+++ week6-20211111/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker pull nginx
+docker pull redis
+docker pull hello-world
+docker pull ubuntu
+docker run -d nginx &
+docker run -d redis &
+clear
+echo "Please wait 10s for the environment setup..."
+sleep 10s
+echo ""
+echo ""
+echo "The test environment is ready! Please go to the quiz to start your test! Good luck!"
+echo "Note: The quiz link should be sent to your email. Please contact Chance if you don't receive it. Thanks"
