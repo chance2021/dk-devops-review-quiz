69efef57021ce1073c9686f63122b7d6d8af6281
chance2021 <61915798+chance2021@users.noreply.github.com>
2021-11-17 06:51:26 -0500

    Update setup.sh

--- week6-20211111/setup.sh
+++ week6-20211111/setup.sh
@@ -4,7 +4,7 @@ docker pull nginx
 docker pull redis
 docker pull hello-world
 docker pull ubuntu
-docker run -d --name dk -p 8888:80 -v /usr/local/share:/etc/nginx nginx &
+docker run -d --name dk -p 8888:80 -v /usr/local/share:/tmp nginx &
 docker run -d --name indoc -p 9999:6379 -p 7777:6378 redis &
 clear
 echo "Please wait 10s for the environment setup..."
