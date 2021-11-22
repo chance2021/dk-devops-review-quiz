afae3ded215c40102ddf0fdd920e2cf67394dd97
chance <cchen@indocresearch.org>
2021-11-22 11:29:01 -0500

    add a text to dk container

--- week7-20211118/setup.sh
+++ week7-20211118/setup.sh
@@ -5,7 +5,7 @@ docker pull redis
 docker pull hello-world
 docker pull ubuntu
 echo "This is week7!" > /usr/local/share/hello.txt
-docker run -d --name dk -p 8888:80 -v /usr/local/share:/etc/nginx nginx &
+docker run -d --name dk -p 8888:80 -v /usr/local/share:/tmp nginx &
 docker run -d --name indoc -p 9999:6379 -p 7777:6378 redis &
 
 clear
