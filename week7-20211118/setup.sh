08e6434921f225830519b4e136b8bdcb7766f388
chance <cchen@indocresearch.org>
2021-11-22 11:21:25 -0500

    Update dk container host volume and reduce the waiting time

--- week7-20211118/setup.sh
+++ week7-20211118/setup.sh
@@ -4,11 +4,11 @@ docker pull nginx
 docker pull redis
 docker pull hello-world
 docker pull ubuntu
-docker run -d --name dk -p 8888:80 -v /usr/local/chance:/etc/nginx nginx &
+docker run -d --name dk -p 8888:80 -v /usr/local/share:/etc/nginx nginx &
 docker run -d --name indoc -p 9999:6379 -p 7777:6378 redis &
 clear
-echo "Please wait 10s for the environment setup..."
-sleep 10s
+echo "Please wait 5s for the environment setup..."
+sleep 5s
 echo ""
 echo ""
 echo "The test environment is ready! Please go to the quiz to start your test! Good luck!"
