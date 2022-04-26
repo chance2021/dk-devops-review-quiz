4292691d28bb2e738d4e09fc07440d68a70bb6d2
chance <cchen@indocresearch.org>
2022-04-26 08:41:34 -0400

    Week 27

--- week27-20220421-ConfigmapSecrets/setup.sh
+++ week27-20220421-ConfigmapSecrets/setup.sh
@@ -10,143 +10,28 @@ kubectl create ns prod
 
 
 # Create Pods
-kubectl run test1 --image=nginx -n mynamespace
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: Pod
 metadata:
-  creationTimestamp: null
-  labels:
-    run: test2
-  name: test2
+  name: ubuntu-sleeper
   namespace: mynamespace
 spec:
   containers:
-  - image: nginx2
-    name: test2
-  - image: hello-world
-    name: test2-1
+  - image: ubuntu
+    name: ubuntu-sleeper
+    command: ["sleep"]
+    args: ["5000"]
 EOF
 
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: Pod
-metadata:
-  creationTimestamp: null
-  labels:
-    run: test3
-    type: ssd
-  name: test3
-  namespace: mynamespace
-spec:
-  containers:
-  - image: redis
-    name: test3
-  - image: nginx
-    name: test3-2
-EOF
-kubectl expose pod test3 --target-port=6379 --port=80 -n mynamespace
-kubectl run test4 --image=redis123 -n mynamespace
-kubectl run test5 --image=hello-world -n mynamespace
-kubectl run test6 --image=centos8 -n mynamespace
-kubectl run blue --image=nginx -n prod
-kubectl run db-service --image=postgres -n prod
-kubectl expose pod db-service -n prod --port=5432 --target-port=5432
-kubectl run db-service --image=postgres -n staging
-kubectl expose pod db-service -n staging --port=5432 --target-port=5432
-kubectl create deployment test7 --image=redis -n mynamespace
-kubectl create deployment test8 --image=nginx -n mynamespace
-kubectl scale deployment test7 --replicas=3 -n mynamespace
+# Create ConfigMap
+kubectl create configmap cmtest1 --from-literal=APP_NAME=red
+kubectl create configmap cmtest2 --from-literal=APP_NAME=green
+kubectl create configmap cmtest3 --from-literal=APP_NAME=blue
 
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: Service
-metadata:
-  name: test9
-  namespace: mynamespace
-  labels:
-    app: test9
-spec:
-  ports:
-  - port: 80
-    name: web
-  clusterIP: None
-  selector:
-    app: test9
----
-apiVersion: apps/v1
-kind: StatefulSet
-metadata:
-  name: test9
-  namespace: mynamespace
-spec:
-  selector:
-    matchLabels:
-      app: test9 
-  serviceName: "test9"
-  replicas: 3 # by default is 1
-  template:
-    metadata:
-      labels:
-        app: test9
-    spec:
-      terminationGracePeriodSeconds: 10
-      containers:
-      - name: test
-        image: k8s.gcr.io/nginx-slim:0.8
-        ports:
-        - containerPort: 80
-          name: web
-EOF
-
-
-cat <<EOF | kubectl apply -f -
-apiVersion: apps/v1
-kind: DaemonSet
-metadata:
-  name: fluentd-elasticsearch
-  namespace: kube-system
-  labels:
-    k8s-app: fluentd-logging
-spec:
-  selector:
-    matchLabels:
-      name: fluentd-elasticsearch
-  template:
-    metadata:
-      labels:
-        name: fluentd-elasticsearch
-    spec:
-      tolerations:
-      # this toleration is to have the daemonset runnable on master nodes
-      # remove it if your masters can't run pods
-      - key: node-role.kubernetes.io/master
-        operator: Exists
-        effect: NoSchedule
-      containers:
-      - name: fluentd-elasticsearch
-        image: quay.io/fluentd_elasticsearch/fluentd:v2.5.2
-        resources:
-          limits:
-            memory: 200Mi
-          requests:
-            cpu: 100m
-            memory: 200Mi
-        volumeMounts:
-        - name: varlog
-          mountPath: /var/log
-        - name: varlibdockercontainers
-          mountPath: /var/lib/docker/containers
-          readOnly: true
-      terminationGracePeriodSeconds: 30
-      volumes:
-      - name: varlog
-        hostPath:
-          path: /var/log
-      - name: varlibdockercontainers
-        hostPath:
-          path: /var/lib/docker/containers
-EOF
+# Create Secrets
+kubectl create secret generic test4-secret --from-literal=username=produser --from-literal=password=Y4nys7f11
+kubectl create secret generic test5-db-secret --from-literal=username=produser --from-literal=password=Y4nys7f11
 
 clear
 echo "Please wait 15s for the environment setup..."
